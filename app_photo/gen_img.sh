0ce53d526130f8a743dcfa2e1306f4055d84f08f
doali <doali>
2024-12-23 08:44:52 +0100

    Script de recuperation des chemins des images

---
+++ app_photo/gen_img.sh
@@ -0,0 +1,6 @@
+# ==============================================================================
+# Generation du fichier images.txt
+# - contenant tous les chemins des images trouvees
+# => a lancer au niveau de galerie/app_photo
+# ==============================================================================
+find static/images/* -iname "*.jpeg" -o -iname "*.jpg" -o -iname "*.png" | grep -vi system >images.txt
