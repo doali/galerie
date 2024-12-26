f2129af8af1389310ce5959baf09254ab250a24e
doali <doali>
2024-12-26 11:06:55 +0100

    Ajout videos

--- app_photo/gen_img.sh
+++ app_photo/gen_img.sh
@@ -3,4 +3,4 @@
 # - contenant tous les chemins des images trouvees
 # => a lancer au niveau de galerie/app_photo
 # ==============================================================================
-find static/images/* -iname "*.jpeg" -o -iname "*.jpg" -o -iname "*.png" | grep -vi system >images.txt
+find static/images/* -iname "*.jpeg" -o -iname "*.jpg" -o -iname "*.png" -o -iname "*.mp4" | grep -vi system >images.txt
