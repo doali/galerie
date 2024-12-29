f97481c8625acc4fed695fb54fa83653758e4b8e
doali <doali>
2024-12-29 10:47:08 +0100

    Correction

--- app_photo/scripts/consultation.sh
+++ app_photo/scripts/consultation.sh
@@ -4,19 +4,20 @@ function usage {
 	echo "$(basename ${0}) <mot-clef> <fichiers>"
 }
 
-[ "${1}" == '--help' ] || [ ${#} == 0 ] && usage && exit 0
+[ "${1}" == '--help' ] || [ ${#} -le 1 ] && usage && exit 0
 
 mot_clef=${1}
 shift
 fichiers=${@}
 
 for fichier in ${fichiers}; do
-	for ch in $(cat ${fichier}); do
-		libelle=$(echo ${ch} | cut -d: -f1)
-		chemin=$(echo ${ch} | cut -d: -f2-)
+    # Utilisation de IFS pour éviter la séparation des chemins contenant des espaces
+    while IFS= read -r ch; do
+        libelle=$(echo "${ch}" | cut -d: -f1)
+        chemin=$(echo "${ch}" | cut -d: -f2-)
 
-		if [[ "${mot_clef}" =~ "${libelle}" ]]; then
-			echo "${ch}" | cut -d: -f 2-
-		fi
-	done
+        if [[ "${mot_clef}" =~ "${libelle}" ]]; then
+            echo "${chemin}" | cut -d: -f 2-
+        fi
+    done < "${fichier}"
 done
